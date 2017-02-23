d6bc89294fa72a56da831e43d9bc47e6f1aff7ec
ccwrc <j.muszelik@gmail.com>
2017-02-23 18:05:09 +0100

    add create category action

--- ccwrcSmallAds/src/SmallAdsBundle/Controller/CategoryController.php
+++ ccwrcSmallAds/src/SmallAdsBundle/Controller/CategoryController.php
@@ -4,24 +4,50 @@ namespace SmallAdsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
-class CategoryController extends Controller
-{
+use SmallAdsBundle\Entity\Category;
+
+/**
+ * @Route("/category")
+ */
+class CategoryController extends Controller {
+    
     /**
      * @Route("/create")
      */
-    public function createAction()
-    {
+    public function createAction(Request $req) {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'Dostęp zabroniony');
+
+        $category = new Category();
+        $form = $this->createFormBuilder($category)
+                ->setMethod("POST")
+                ->add("name", "text", ["label" => "Podaj nazwę kategorii"])
+                ->add("save", "submit", ["label" => "Zapisz"])
+                ->getForm();
+
+        $form->handleRequest($req);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $category = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($category);
+            $em->flush();
+            return $this->redirectToRoute("smallads_category_showall", [
+                            //
+            ]);
+        }
+
         return $this->render('SmallAdsBundle:Category:create.html.twig', array(
-            // ...
+                    "form" => $form->createView()
         ));
     }
 
     /**
      * @Route("/edit")
      */
-    public function editAction()
-    {
+    public function editAction() {
+        //
+        
         return $this->render('SmallAdsBundle:Category:edit.html.twig', array(
             // ...
         ));
@@ -30,18 +56,31 @@ class CategoryController extends Controller
     /**
      * @Route("/show")
      */
-    public function showAction()
-    {
+    public function showAction() {
+        //
+        
         return $this->render('SmallAdsBundle:Category:show.html.twig', array(
             // ...
+        ));
+    }
+    
+    /**
+     * @Route("/showAll")
+     */
+    public function showAllAction() {
+        //
+
+        return $this->render('SmallAdsBundle:Category:show_all.html.twig', array(
+                        // ...
         ));
     }
 
     /**
      * @Route("/delete")
      */
-    public function deleteAction()
-    {
+    public function deleteAction() {
+        //
+        
         return $this->render('SmallAdsBundle:Category:delete.html.twig', array(
             // ...
         ));
--- ccwrcSmallAds/src/SmallAdsBundle/Controller/CommentController.php
+++ ccwrcSmallAds/src/SmallAdsBundle/Controller/CommentController.php
@@ -5,6 +5,9 @@ namespace SmallAdsBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+/**
+ * @Route("/comment")
+ */
 class CommentController extends Controller
 {
     /**
