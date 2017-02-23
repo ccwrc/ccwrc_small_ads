74a05042db59b442ba444075b4881a8caf2d8a2c
ccwrc <j.muszelik@gmail.com>
2017-02-23 15:56:37 +0100

    add comment controller

---
+++ ccwrcSmallAds/src/SmallAdsBundle/Controller/CommentController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace SmallAdsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class CommentController extends Controller
+{
+    /**
+     * @Route("/create")
+     */
+    public function createAction()
+    {
+        return $this->render('SmallAdsBundle:Comment:create.html.twig', array(
+            // ...
+        ));
+    }
+
+    /**
+     * @Route("/edit")
+     */
+    public function editAction()
+    {
+        return $this->render('SmallAdsBundle:Comment:edit.html.twig', array(
+            // ...
+        ));
+    }
+
+    /**
+     * @Route("/show")
+     */
+    public function showAction()
+    {
+        return $this->render('SmallAdsBundle:Comment:show.html.twig', array(
+            // ...
+        ));
+    }
+
+    /**
+     * @Route("/showAll")
+     */
+    public function showAllAction()
+    {
+        return $this->render('SmallAdsBundle:Comment:show_all.html.twig', array(
+            // ...
+        ));
+    }
+
+    /**
+     * @Route("/delete")
+     */
+    public function deleteAction()
+    {
+        return $this->render('SmallAdsBundle:Comment:delete.html.twig', array(
+            // ...
+        ));
+    }
+
+}
