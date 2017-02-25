4bd29d5861b4e1e47540d22bae0f80fc89449706
ccwrc <j.muszelik@gmail.com>
2017-02-25 23:07:31 +0100

    backup

--- ccwrcSmallAds/src/SmallAdsBundle/Controller/CategoryController.php
+++ ccwrcSmallAds/src/SmallAdsBundle/Controller/CategoryController.php
@@ -4,47 +4,97 @@ namespace SmallAdsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
-class CategoryController extends Controller
-{
+use SmallAdsBundle\Entity\Category;
+
+class CategoryController extends Controller {
+    
     /**
      * @Route("/createCategory")
      */
-    public function createCategoryAction()
-    {
+    public function createCategoryAction(Request $req) {
+        $this->denyAccessUnlessGranted("ROLE_ADMIN", null, "Dostęp zabroniony");
+
+        $category = new Category();
+        $form = $this->createFormBuilder($category)
+                ->setMethod("POST")
+                ->add("name", "text", ["label" => "Podaj nazwę kategorii: "])
+                ->add("save", "submit", ["label" => "Zapisz"])
+                ->getForm();
+
+        $form->handleRequest($req);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $category = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($category);
+            $em->flush();
+            return $this->redirectToRoute("smallads_category_showallcategories");
+        }
+
         return $this->render('SmallAdsBundle:Category:create_category.html.twig', array(
-            // ...
+                    "form" => $form->createView()
         ));
     }
 
     /**
-     * @Route("/editCategory")
+     * @Route("/{id}/editCategory", requirements={"id"="\d+"})
      */
-    public function editCategoryAction()
-    {
+    public function editCategoryAction(Request $req, $id) {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'Dostęp zabroniony');
+        $category = $this->getDoctrine()->getRepository("SmallAdsBundle:Category")->find($id);
+
+        if ($category == null) {
+            throw $this->createNotFoundException("Brak ID w bazie");
+        }
+
+        $form = $this->createFormBuilder($category)
+                ->setMethod("POST")
+                ->add("name", "text", ["label" => "Edytuj nazwę kategorii: "])
+                ->add("save", "submit", ["label" => "Zapisz"])
+                ->getForm();
+
+        $form->handleRequest($req);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $category = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+            return $this->redirectToRoute("smallads_category_showallcategories");
+        }
+
         return $this->render('SmallAdsBundle:Category:edit_category.html.twig', array(
-            // ...
+                    "form" => $form->createView()
         ));
     }
 
     /**
      * @Route("/showAllCategories")
      */
-    public function showAllCategoriesAction()
-    {
+    public function showAllCategoriesAction() {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'Dostęp zabroniony');
+        $categories = $this->getDoctrine()->getRepository("SmallAdsBundle:Category")->findAll();
+
         return $this->render('SmallAdsBundle:Category:show_all_categories.html.twig', array(
-            // ...
+                    "categories" => $categories
         ));
     }
 
     /**
-     * @Route("/deleteCategory")
+     * @Route("/{id}/deleteCategory", requirements={"id"="\d+"})
      */
-    public function deleteCategoryAction()
-    {
-        return $this->render('SmallAdsBundle:Category:delete_category.html.twig', array(
-            // ...
-        ));
+    public function deleteCategoryAction($id) {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'Dostęp zabroniony');
+        $category = $this->getDoctrine()->getRepository("SmallAdsBundle:Category")->find($id);
+
+        if ($category == null) {
+            throw $this->createNotFoundException("Brak ID w bazie");
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($category);
+        $em->flush();
+
+        return $this->redirectToRoute("smallads_category_showallcategories");
     }
 
 }
