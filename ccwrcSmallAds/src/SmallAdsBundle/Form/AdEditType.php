b6b217b1d3ce2d364a8496aca74e0be9095b2f7b
ccwrc <j.muszelik@gmail.com>
2017-03-05 10:37:22 +0100

    form class adType and adEditType - done

---
+++ ccwrcSmallAds/src/SmallAdsBundle/Form/AdEditType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace SmallAdsBundle\Form;
+
+// use SmallAdsBundle\Entity\Ad;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use \DateTime;
+
+class AdEditType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+                ->add("title", "text", ["label" => "Edytuj tytuł: "])
+                ->add("description", "textarea", ["label" => "Edytuj opis: "])
+                ->add("category", EntityType::class, [
+                    "class" => "SmallAdsBundle:Category", "choice_label" => "name",
+                    "label" => "Zmień kategorię: "])
+                ->add("photoPath", "file", ["label" => "Wgraj inne zdjęcie: ",
+                    "data_class" => null,
+                    "required" => false]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver) {
+        $resolver->setDefaults(array(
+            'data_class' => 'SmallAdsBundle\Entity\Ad',
+        ));
+    }
+
+}
--- ccwrcSmallAds/src/SmallAdsBundle/Form/AdType.php
+++ ccwrcSmallAds/src/SmallAdsBundle/Form/AdType.php
@@ -3,30 +3,44 @@
 namespace SmallAdsBundle\Form;
 
 use SmallAdsBundle\Entity\Ad;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+//use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+//use Symfony\Component\Form\Extension\Core\Type\EmailType;
+//use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use \DateTime;
 
 class AdType extends AbstractType {
     
-//    public function buildForm(FormBuilderInterface $builder, array $options)
-//    {
-//        $builder
-//            ->add('title')
-//            ->add('summary', TextareaType::class)
-//            ->add('content', TextareaType::class)
-//            ->add('authorEmail', EmailType::class)
-//            ->add('publishedAt', DateTimeType::class)
-//        ;
-//    }
-//
-//    public function configureOptions(OptionsResolver $resolver)
-//    {
-//        $resolver->setDefaults(array(
-//            'data_class' => Post::class,
-//        ));
-//    }
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+                ->add("title", "text", ["label" => "Podaj tytuł: "])
+                ->add("description", "textarea", ["label" => "Dodaj opis: "])
+                ->add("category", EntityType::class, [
+                    "class" => "SmallAdsBundle:Category", "choice_label" => "name",
+                    "label" => "Wybierz kategorię: "])
+                ->add("photoPath", "file", ["label" => "Wgraj foto (max. 1280x1024px)",
+                    "data_class" => null,
+                    "required" => false])
+                ->add("endDate", "choice", [
+                    "choices" => [
+                        "3 dni" => new dateTime(date("Y-m-d H:i:s", time() + 3600 * 24 * 3)),
+                        "4 dni" => new dateTime(date("Y-m-d H:i:s", time() + 3600 * 24 * 4)),
+                        "5 dni" => new dateTime(date("Y-m-d H:i:s", time() + 3600 * 24 * 5)),
+                        "6 dni" => new dateTime(date("Y-m-d H:i:s", time() + 3600 * 24 * 6)),
+                        "tydzień" => new dateTime(date("Y-m-d H:i:s", time() + 3600 * 24 * 7))
+                    ],
+                    "choices_as_values" => true, "label" => "Czas trwania: "]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver) {
+        $resolver->setDefaults(array(
+            'data_class' => 'SmallAdsBundle\Entity\Ad',
+        ));
+    }
+    
+    
+
 }
